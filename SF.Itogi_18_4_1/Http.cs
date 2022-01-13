6d705eb2a60bff7f8751c057c93335accf4ff01a
Irina Lokosova <persik2466@gmail.com>
2022-01-13 20:41:43 +0400

    SF.Itogi_18_4_1

---
+++ SF.Itogi_18_4_1/Http.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+
+namespace SF.Itogi_18_4_1
+{
+    internal static class Http
+    {
+        private static readonly Lazy<HttpClient> HttpClientLazy = new(() =>
+        {
+            var handler = new HttpClientHandler
+            {
+                // https://github.com/Tyrrrz/YoutubeExplode/issues/530
+                UseCookies = false
+            };
+
+            if (handler.SupportsAutomaticDecompression)
+                handler.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+
+            return new HttpClient(handler, true);
+        });
+
+        public static HttpClient Client => HttpClientLazy.Value;
+
+        public static Lazy<HttpClient> HttpClientLazy1 => HttpClientLazy;
+    }
+}
