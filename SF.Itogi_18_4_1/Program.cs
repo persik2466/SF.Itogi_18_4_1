ec4856f058ecf4338c2572c7dfab47289a36f6e6
Irina Lokosova <persik2466@gmail.com>
2022-01-13 20:25:34 +0400

    Добавьте файлы проекта.

---
+++ SF.Itogi_18_4_1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+using YoutubeExplode;
+
+
+namespace SF.Itogi_18_4_1
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var youtube = new YoutubeClient();
+
+            // You can specify both video ID or URL
+            var video = await youtube.Videos.GetAsync("https://youtube.com/watch?v=u_yIGGhubZs");
+            Console.WriteLine($"Название: {video.Title}");
+            Console.WriteLine($"Продолжительность: {video.Duration}");
+            Console.WriteLine($"Автор: {video.Author}");
+
+        }
+    }
+}
---
+++ SF.Itogi_18_4_1/YoutubeClient.cs
@@ -0,0 +1,43 @@
+﻿using System.Net.Http;
+using YoutubeExplode.Channels;
+using YoutubeExplode.Playlists;
+using YoutubeExplode.Search;
+using YoutubeExplode.Utils;
+using YoutubeExplode.Videos;
+using static System.Net.WebRequestMethods;
+
+namespace SF.Itogi_18_4_1
+{
+    public class YoutubeClient
+    {
+        public VideoClient Videos { get; }
+        public PlaylistClient Playlists { get; }
+
+        /// <summary>
+        /// Operations related to YouTube channels.
+        /// </summary>
+        public ChannelClient Channels { get; }
+
+        /// <summary>
+        /// Operations related to YouTube search.
+        /// </summary>
+        public SearchClient Search { get; }
+
+        /// <summary>
+        /// Initializes an instance of <see cref="YoutubeClient"/>.
+        /// </summary>
+        public YoutubeClient(HttpClient http)
+        {
+            Videos = new VideoClient(http);
+            Playlists = new PlaylistClient(http);
+            Channels = new ChannelClient(http);
+            Search = new SearchClient(http);
+        }
+        public YoutubeClient() : this(Http.Client)
+        {
+        }
+
+    }
+}
+    
+
